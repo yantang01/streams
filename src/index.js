9e74933ce407c4f2d2a562012f2234b683a6d31b
yantang01 <yantang0727@gmail.com>
2022-09-24 15:04:48 -0400

    Set up Redux DevTools

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 
 import App from "./components/App";
 import reducers from "./reducers";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={createStore(reducers)}>
+    <Provider
+      store={createStore(reducers, composeEnhancers(applyMiddleware()))}
+    >
       <App />
     </Provider>
   </React.StrictMode>
