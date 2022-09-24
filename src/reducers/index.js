eea903458014f89863163aed40a184bbc6bc56bb
yantang01 <yantang0727@gmail.com>
2022-09-24 15:18:22 -0400

    Install Redux-Form

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
+// this reducer is from Redux-Form library, we did not create this reducer
+// our goal is to combine this reducer to our reducers
+import { reducer as formReducer } from "redux-form";
 import authReducer from "./authReducer";
 
 export default combineReducers({
   auth: authReducer,
+  form: formReducer,
 });
