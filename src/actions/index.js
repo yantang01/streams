5944f4552ecbe33a728446ce8759de91b07b2656
yantang01 <yantang0727@gmail.com>
2022-09-24 13:48:20 -0400

    fix action types

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,15 @@
 // can only change states in action creators
+import { SIGN_IN } from "./types";
+import { SIGN_OUT } from "./types";
 
 export const signIn = () => {
   return {
-    type: "SIGN_IN",
+    type: SIGN_IN,
   };
 };
 
 export const signOut = () => {
   return {
-    type: "SIGN_OUT",
+    type: SIGN_OUT,
   };
 };
