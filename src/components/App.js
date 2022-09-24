f5b0d24224f1bfdbce79fecd238dabd51b037c1b
yantang01 <yantang0727@gmail.com>
2022-09-24 00:47:20 -0400

    Set Up Header Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import StreamCreate from "./streams/StreamCreate";
 import StreamEdit from "./streams/StreamEdit";
 import StreamDelete from "./streams/StreamDelete";
 import StreamShow from "./streams/StreamShow";
 import StreamList from "./streams/StreamList";
+import Header from "./Header";
 
 function App() {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
         <div>
+          <Header />
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
           <Route path="/streams/edit" exact component={StreamEdit} />
