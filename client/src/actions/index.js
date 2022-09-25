347063c67e246ef289c411a2cdec8f57364a6c20
yantang01 <yantang0727@gmail.com>
2022-09-25 11:13:36 -0400

    Update createStream and editStream forms

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -56,11 +56,15 @@ export const fetchStream = (id) => {
   };
 };
 
+// Put request will update ALL properties of a record
+// Use Patch! to update SOME properties
 export const editStream = (id, formValues) => {
   return async (dispatch) => {
-    const response = await streams.put(`/streams/${id}`, formValues);
+    const response = await streams.patch(`/streams/${id}`, formValues);
 
     dispatch({ type: EDIT_STREAM, payload: response.data });
+
+    history.push("/");
   };
 };
 
--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,44 @@
+import _ from "lodash";
 import React from "react";
+import { connect } from "react-redux";
+import { fetchStream, editStream } from "../../actions";
+import StreamForm from "./StreamForm";
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+// With React Router, each component needs to be designed to work in isolation
+// fetch its own data
+
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onSubmit = (formValues) => {
+    this.props.editStream(this.props.match.params.id, formValues);
+  };
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading</div>;
+    }
+    return (
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          onSubmit={this.onSubmit}
+          initialValues={_.pick(this.props.stream, "title", "description")}
+        />
+      </div>
+    );
+  }
+}
+
+// ownProps is the props in the StreamEdit component
+// created by React Router
+const mapToStateProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamEdit;
+export default connect(mapToStateProps, {
+  fetchStream,
+  editStream,
+})(StreamEdit);
